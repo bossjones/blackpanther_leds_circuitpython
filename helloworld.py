b540345c6b9ad5a53160d5fb7a308df4cec66c2b
Malcolm Jones <bossjones@theblacktonystark.com>
2018-11-06 01:57:10 -0500

    WOOT: the whole unit worksssssss

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,6 @@
+# NOTE: Keeping this commented out fixes our memory issue!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # import time
 
-# print("HELLO WORLD %sms" % (time.monotonic() / 1000))
+# print("HELLO WORLD %sms" % (time.monotonic() / 1000))  # memory issue might be due to using %sms instead of {} and format FYI.
 
 print("HELLO WORLD")
